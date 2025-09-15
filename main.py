a9cbb82e7d914d686fd99d18eb274c96dd3abfdf
mdrashid93 <md.rasshid9@gmail.com>
2025-09-15 11:36:26 +0530

    Add files via upload
    api

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse, RedirectResponse,JSONResponse
-from src.routes.auth import router
+import streamlit as st
+from supabase import create_client
+data=create_client("")
+
+tabs=st.tabs(['login','signup'])
+ 
 
 
-app=FastAPI()
-
-
-# @app.get('/')
-# def hello():
-#     # return HTMLResponse('''
-#     #                     <h1>hello<h1>
-#     #                     ''')
-#     # return JSONResponse({
-#     #     "message":"hello"
-#     # }) 
-#     return RedirectResponse("https://medhaedutech.com")
-
-
-app.include_router(router)
+with tabs[0]:
+    email=st.text_input('username',placeholder='placeholder')
+    pasword=st.text_input('password',placeholder='password')
+    signup=st.button('login',type='primary',use_container_width=True)
+    if signup_btn:
+        response = supabase.auth.sign_up({
+        "email": email,
+        "password": password
+         }
+        )
+        st.write(response.user)
+        
+with tabs[1]:
+    username=st.text_input('username',placeholder='placeholder')
+    password=st.text_input('password',placeholder='password',type='password' )
+    login=st.button('login',type='primary',use_container_width=True)
+    if login_btn:
+       # database.auth.sign_in_with_password 
+        res=response = supabase.auth.sign_up({
+        "email": email,
+        "password": password
+         }
+        )
+        st.write(res.session.access_token)
+        pg=st.navigation([st.page("home.py"),st.page("page_2.py")])
